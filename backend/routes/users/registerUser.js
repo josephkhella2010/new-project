55bbf7e72c76afa8f71a73baa91812a1363ed0f0
joseph khella <josephkhella2010@gmail.com>
2026-07-25 20:50:51 +0200

    add register and mongoos

---
+++ backend/routes/users/registerUser.js
@@ -0,0 +1,117 @@
+import express from "express";
+import User from "../../Mongoo/schemas/User.js";
+
+const router = express.Router();
+
+router.post("/register-user", async (req, res) => {
+  try {
+    // function
+    const transferName = (name, isLower = false) => {
+      if (!name) return "";
+
+      if (isLower) {
+        return name.trim().toLowerCase();
+      }
+
+      return name.trim();
+    };
+
+    // get data from body and format it
+    const firstName = transferName(req.body.firstName, true);
+    const lastName = transferName(req.body.lastName, true);
+    const username = transferName(req.body.username, true);
+    const email = transferName(req.body.email, true);
+    const dateOfBirth = req.body.dateOfBirth;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    const fieldsArr = [
+      "firstName",
+      "lastName",
+      "username",
+      "email",
+      "dateOfBirth",
+      "password",
+      "confirmPassword",
+    ];
+
+    const Fields = [];
+    const sms = [];
+
+    const allFieldsEmpty = fieldsArr.every((field) => {
+      return !req.body[field];
+    });
+
+    if (allFieldsEmpty) {
+      return res.status(400).json({
+        message: ["Please fill all your fields"],
+        Fields: fieldsArr,
+      });
+    }
+
+    for (const field of fieldsArr) {
+      if (!req.body[field]) {
+        sms.push(`Please Fill ${field}`);
+        Fields.push(field);
+      }
+    }
+
+    if (Fields.length > 0) {
+      return res.status(400).json({
+        message: sms,
+        Fields,
+      });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        message: ["Password and confirm password do not match"],
+        fields: ["password", "confirmPassword"],
+      });
+    }
+
+    const existUserUsername = await User.findOne({ username });
+    const existUserEmail = await User.findOne({ email });
+
+    if (existUserUsername && existUserEmail) {
+      return res.status(400).json({
+        message: ["Username is already exist", "Email is already exist"],
+        fields: ["username", "email"],
+      });
+    }
+
+    if (existUserUsername) {
+      return res.status(400).json({
+        message: ["Username is already exist"],
+        fields: ["username"],
+      });
+    }
+
+    if (existUserEmail) {
+      return res.status(400).json({
+        message: ["Email is already exist"],
+        fields: ["email"],
+      });
+    }
+
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      username,
+      email,
+      dateOfBirth,
+      password,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./Mongoo/config/db.js";
+import registerRouter from "./routes/users/registerUser.js";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+// Connect Database
+connectDB();
+
+// api
+app.use("/api", registerRouter);
+
+//
 
 const PORT = process.env.PORT || 5200;
 app.get("/", (req, res) => {
